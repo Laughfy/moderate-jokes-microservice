0ee232f16b420279214a2d6e5328eaa4e9c90d29
SKSritharan <sk.sritharan75@gmail.com>
2024-11-06 22:30:41 +0530

    Fixed bearer token extract

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,11 +2,16 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 function authenticateToken(req, res, next) {
-  const token = req.headers["authorization"];
-  if (!token) return res.status(401).send("Access Denied");
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send("Access Denied");
+  }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).send("Invalid Token");
+
     req.user = user;
     next();
   });
